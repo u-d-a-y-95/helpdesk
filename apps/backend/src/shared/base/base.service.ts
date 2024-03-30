ff48a606b375a4409f5dbccacc20af4fbf136682
UDay <uday.dev.bd@gmail.com>
2024-03-30 16:34:34 +0600

    refactor base service

--- apps/backend/src/shared/base/base.service.ts
+++ apps/backend/src/shared/base/base.service.ts
@@ -1,41 +1,50 @@
-import { DeepPartial, FindOptionsWhere, Repository } from 'typeorm';
+import {
+  DeepPartial,
+  FindManyOptions,
+  FindOneOptions,
+  FindOptionsWhere,
+  Repository,
+} from 'typeorm';
 import { BaseEntity } from './base.entity';
 import { NotFoundException } from '@nestjs/common';
 
 export class BaseService<T extends BaseEntity> {
   constructor(private repository: Repository<T>) {}
 
-  private createInstance(values) {
-    return this.repository.create(values);
+  private async createInstance(values): Promise<any> {
+    const instance = await this.repository.create(values);
+    return instance;
   }
 
   async create(values: DeepPartial<T>) {
-    const instance = this.createInstance(values);
+    const instance: T = await this.createInstance(values);
     const row = await this.repository.save(instance);
     return row;
   }
 
-  find() {
-    return this.repository.find();
+  find(options: FindManyOptions<T> = {}) {
+    return this.repository.find(options);
   }
 
-  async findById(id: string) {
-    const record = await this.repository.findOneBy({
-      id,
-    } as FindOptionsWhere<T>);
+  async findById(id: string, options: Omit<FindOneOptions<T>, 'where'> = {}) {
+    const record = await this.repository.findOne({
+      where: { id },
+      ...options,
+    } as FindOneOptions<T>);
     if (!record) throw new NotFoundException();
     return record;
   }
 
   async updateById(id: string, values: DeepPartial<T>) {
     const oldRecord = await this.findById(id);
-    const instance = this.createInstance({
+    const instance = await this.createInstance({
       id,
       ...values,
     });
     const record = await this.repository.save(instance);
     return Object.assign(oldRecord, record);
   }
+
   async removeById(id: string) {
     const record = await this.findById(id);
     await this.repository.delete({ id } as FindOptionsWhere<T>);
